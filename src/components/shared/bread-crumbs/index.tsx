dd92bd244ffa3e124211b2ec3c6d0fff9604561b
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-23 22:51:22 +0330

    feat: Build BreadCrumbs component

---
+++ src/components/shared/bread-crumbs/index.tsx
@@ -0,0 +1,39 @@
+import { Breadcrumbs, Link, Stack } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import React from "react";
+
+type BreadCrumbsProps = {
+  array: [string, string][];
+};
+
+export default function BreadCrumbs({ array }: BreadCrumbsProps) {
+  const breadcrumbs = array.map((item, index) => {
+    const isLast = index === array.length - 1;
+    return (
+      <Link
+        sx={{
+          fontSize: "18px",
+          color: isLast ? "blue" : "inherit",
+          textDecoration: isLast ? "underline" : "none",
+        }}
+        underline="hover"
+        key={index + 1}
+        href={item[1]}
+      >
+        {item[0]}
+      </Link>
+    );
+  });
+  return (
+    <Stack sx={{ mb: "40px", pt: "24px" }} spacing={2}>
+      <Breadcrumbs
+        separator={
+          <NavigateNextIcon fontSize="large" sx={{ color: "#717171" }} />
+        }
+        aria-label="breadcrumb"
+      >
+        {breadcrumbs}
+      </Breadcrumbs>
+    </Stack>
+  );
+}
