7e0f3f68952bd2071684a67a51255a4342fe8abe
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-23 22:50:48 +0330

    feat: Add account page constants

---
+++ src/constants/accountSidebar.ts
@@ -0,0 +1,19 @@
+import personalData from "@/assets/images/account-sidebar-icons/user-edit.svg";
+import orders from "@/assets/images/account-sidebar-icons/bag.svg";
+import wishlist from "@/assets/images/account-sidebar-icons/heart.svg";
+import discounts from "@/assets/images/account-sidebar-icons/gift.svg";
+import security from "@/assets/images/account-sidebar-icons/security-safe.svg";
+import notifications from "@/assets/images/account-sidebar-icons/notification.svg";
+import contactUs from "@/assets/images/account-sidebar-icons/24-support.svg";
+import logOut from "@/assets/images/account-sidebar-icons/logout.svg";
+
+export const accountSidebar = [
+  [personalData, "Personal Data"],
+  [orders, "Orders"],
+  [wishlist, "Wishlist"],
+  [discounts, "Discounts"],
+  [security, "Security & Access"],
+  [notifications, "Notifications"],
+  [contactUs, "Contact Us"],
+  [logOut, "Log Out"],
+];
