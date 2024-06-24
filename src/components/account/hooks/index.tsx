37f0e6df873069f1f6c1d17063a8696a6d0c9959
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-24 17:44:09 +0330

    feat: Add account hooks and services

--- src/components/account/hooks/index.tsx
+++ src/components/account/hooks/index.tsx
@@ -1,9 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-import { getUserInfo } from "../services";
+import { getUserInfo, getUserWishlistItems } from "../services";
 
 export const useGetUserInfo = () => {
   return useQuery({
     queryKey: ["user-info"],
     queryFn: () => getUserInfo(),
+  });
+};
+
+export const useGetUserWishlist = () => {
+  return useQuery({
+    queryKey: ["user-wishlist"],
+    queryFn: () => getUserWishlistItems(),
   });
 };
--- src/components/account/services/index.ts
+++ src/components/account/services/index.ts
@@ -1,3 +1,4 @@
+import { IProduct } from "@/components/home/hooks/types";
 import { BASE_URL } from "@/constants/urls";
 import { fetchIdCookie } from "@/layout/navbar/services";
 import axios from "axios";
@@ -6,4 +7,27 @@ export const getUserInfo = async () => {
   const userId = fetchIdCookie();
   const response = await axios.get(`${BASE_URL}/users/${userId}`);
   return response.data;
+};
+
+export const getUserWishlistItems = async () => {
+  const userId = fetchIdCookie();
+  const response = await axios.get(`${BASE_URL}/wishlist/${userId}`);
+  const idOfItemsInWishlist: number[] = response.data.wishlistProducts;
+  const wishlistProducts: IProduct[] = [];
+
+  await Promise.all(
+    idOfItemsInWishlist.map(async (productId) => {
+      try {
+        const productResponse = await axios.get<IProduct>(
+          `${BASE_URL}/products/${productId}`
+        );
+        const product = productResponse.data;
+        wishlistProducts.push(product);
+      } catch (error) {
+        console.error(`Error fetching product ${productId}:`, error);
+      }
+    })
+  );
+
+  return wishlistProducts;
 };
