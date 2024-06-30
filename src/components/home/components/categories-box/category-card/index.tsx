cfe98ec45326c68051e37e6b36d9315879fce607
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-30 19:28:15 +0330

    feat: Modify CategoryCard component

--- src/components/home/components/categories-box/category-card/index.tsx
+++ src/components/home/components/categories-box/category-card/index.tsx
@@ -4,50 +4,47 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Box, CardActionArea } from "@mui/material";
 import { StaticImageData } from "next/image";
+import Link from "next/link";
 
 interface CategoriesType {
   id?: number;
   image?: StaticImageData;
   title?: string;
+  url: string;
 }
 
-export default function CategoryCard({ image, title }: CategoriesType) {
+export default function CategoryCard({ image, title, url }: CategoriesType) {
+  const imgSrc = image ? image.src : "";
   return (
-    <Card
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        alignItems: "center",
-        // maxWidth: 190,
-        // minWidth: 184,
-        m: 1,
-        // width: 184,
-        // height: 200,
-        // maxHeight: 200,
-        px: 3,
-      }}
-    >
-      <CardActionArea>
-        <Box component="img" src={image.src} alt="category" />
-        <CardContent>
-          {/* <Link href={"/"}> */}
-          <Typography
-            gutterBottom
-            variant="body1"
-            component="h6"
-            color="GrayText"
-            sx={{
-              fontSize: 16,
-              textAlign: "center",
-              ":hover": { textDecoration: "underline" },
-            }}
-          >
-            {title}
-          </Typography>
-          {/* </Link> */}
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link href={`/products/${url}`}>
+      <Card
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          m: 1,
+        }}
+      >
+        <CardActionArea>
+          <Box sx={{ px: 3 }} component="img" src={imgSrc} alt="category" />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="body1"
+              component="h6"
+              color="GrayText"
+              sx={{
+                fontSize: 16,
+                textAlign: "center",
+                ":hover": { textDecoration: "underline" },
+              }}
+            >
+              {title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
