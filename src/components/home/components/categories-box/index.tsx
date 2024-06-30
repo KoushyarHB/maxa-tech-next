0711594d3f71e5c870f27fd5bf977f5b40de3888
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-30 19:31:18 +0330

    fix: Modify landing page CategoriesBox component

--- src/components/home/components/categories-box/index.tsx
+++ src/components/home/components/categories-box/index.tsx
@@ -1,44 +1,48 @@
-import { useEffect, useState } from "react";
-import { Box, Container, Typography } from "@mui/material";
-import CategoryCard from "./category-card";
-
 import accessories from "@/assets/images/categories-images/accessories.png";
 import camera from "@/assets/images/categories-images/camera.png";
+import gaming from "@/assets/images/categories-images/gaming.png";
 import laptop from "@/assets/images/categories-images/laptop.png";
 import smartPhone from "@/assets/images/categories-images/smartphone.png";
-import gaming from "@/assets/images/categories-images/gaming.png";
 import smartWatch from "@/assets/images/categories-images/smartwatch.png";
+import { Box, Container } from "@mui/material";
+import CategoryCard from "./category-card";
 
 const categories = [
   {
     id: "1",
     image: accessories,
     title: "Accessories",
+    url: "accessories",
   },
   {
     id: "2",
     image: camera,
     title: "Camera",
+    url: "camera",
   },
   {
     id: "3",
     image: laptop,
     title: "Laptop",
+    url: "laptop",
   },
   {
     id: "4",
     image: smartPhone,
     title: "Smart Phone",
+    url: "smart-phone",
   },
   {
     id: "5",
     image: gaming,
     title: "Gaming",
+    url: "gaming",
   },
   {
     id: "6",
     image: smartWatch,
     title: "Smart Watch",
+    url: "smart-watch",
   },
 ];
 
@@ -58,6 +62,7 @@ export default function CategoriesBox() {
             key={category.id}
             image={category.image}
             title={category.title}
+            url={category.url}
           />
         ))}
       </Box>
